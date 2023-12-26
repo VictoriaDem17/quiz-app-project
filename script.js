dcc1d1770c2937ad79a68788c337fd18d9f43a11
Viki <victoriadem.317@gmail.com>
2023-12-26 20:22:14 +0100

    javascript2-week2/Filter function, next-side btn

--- script.js
+++ script.js
@@ -73,3 +73,78 @@ function clearForm() {
       radio.checked = false;
   });
 }
+
+//JS2-W2-3
+//MyQuestions
+/*
+..some function openNextForm(), onclick button "Next side" will open new windows with empty formDiv and h1,
+and shows imput for searching questions by content and new button "Search"..
+
+const quizData = [
+    {
+    question: "Who is the creator of JavaScript?",
+    options: [
+      { text: "Guido van Rossum", isCorrect: false },
+      { text: "James Gosling", isCorrect: false },
+      { text: "Brendan Eich", isCorrect: true },
+      { text: "Bjarne Stroustrup", isCorrect: false },
+    ],
+  },
+  {
+    question: "What was JavaScript called in September 1995?",
+    options: [
+      { text: "LiveScript", isCorrect: false },
+      { text: "Java", isCorrect: false },
+      { text: "Latte Macchiato", isCorrect: false },
+      { text: "Mocha", isCorrect: true },
+    ],
+  },
+  {
+    question: "Which JavaScript statement is incorrect?",
+    options: [
+      { text: '"b" + "a" + + "a" + "a" //output "baNaNa"', isCorrect: false },
+      { text: "Number.MIN_VALUE > 0 //true", isCorrect: true },
+      { text: "functions can execute themselves", isCorrect: false },
+      { text: "undefined never can be defined", isCorrect: false },
+    ],
+  },
+  {
+    question: "Which JavaScript syntax is correct?",
+    options: [
+      { text: 'return {""};', isCorrect: false },
+      { text: '1 + 2 + "3" //output "123"', isCorrect: false },
+      { text: "[,*,].length", isCorrect: false },
+      { text: ";let a = 2", isCorrect: true },
+    ],
+  },
+  {
+    question: "How many years in the row JavaScript keep being the most popular programming language between developers?",
+    options: [
+      { text: "10 years", isCorrect: false },
+      { text: "11 years", isCorrect: true },
+      { text: "12 years", isCorrect: false },
+      { text: "13 years", isCorrect: false },
+    ],
+  },
+];
+
+function filterQuestions(searchContent) {
+    const filteredQuestions = quizData.filter((question) =>
+      question.question.toLowerCase().includes(searchContent.toLowerCase())
+    );
+    
+    filteredQuestions.forEach((question) => {
+    const optionsHTML = question.options
+        .map(
+        (option) =>
+        `<div class="option"><input type="radio" name="${question.question}" value="${option.text}"> ${option.text}</div>`
+        )
+        .join("");
+
+    const questionHTML = 
+    `<div class="question"><h3>${question.question}</h3>${optionsHTML}</div>`;
+
+    document.getElementById("quiz").innerHTML += questionHTML;
+});
+}
+*/
